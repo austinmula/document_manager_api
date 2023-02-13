116c36573a9ec3da9038824e26aaef9fa7652c05
cymula <amula@cytonn.com>
2023-02-13 17:35:49 +0300

    seeding file categories

---
+++ app/Models/FileCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FileCategory extends Model
+{
+    use HasFactory;
+    protected $table = 'file_categories';
+}
