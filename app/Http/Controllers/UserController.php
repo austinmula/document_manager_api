c9ec9ee70873edfa7522ed956bd1d8b2b5d28822
cymula <amula@cytonn.com>
2023-02-15 16:05:58 +0300

    default login credentials email completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NewUserCreated;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -26,21 +28,25 @@ class UserController extends Controller
             'email' => 'required|unique:users|email|max:255',
         ]);
 
+        $password = Str::random(8);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password),
+            'password' => bcrypt($password),
             'role_id'=> $request->role,
             'department_id'=> $request->department
         ]);
 
-//        send user an email
 
-        if ($user)
+        if ($user){
+            event(new NewUserCreated($request->input('email'), $password));
             return response()->json([
                 'success' => true,
                 'data' => $user->toArray()
             ]);
+        }
+
         else
             return response()->json([
                 'success' => false,
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\FilesController;
 use App\Http\Controllers\PassportController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RequestController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,7 @@ Route::resource('files', FilesController::class)->middleware('auth:api');
 Route::resource('requests', RequestController::class)->middleware('auth:api');
 Route::resource('departments', DepartmentController::class);
 Route::resource('permissions', PermissionController::class);
+Route::resource('users', UserController::class);
 
 Route::group(['middleware' => 'role:admin'], function() {
     Route::get('/user', function() {
