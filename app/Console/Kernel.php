49c844d00872a740b225d72b4f67e63227a0b217
cymula <amula@cytonn.com>
2023-02-16 08:42:35 +0300

    scheduler first one

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,13 +22,12 @@ class Kernel extends ConsoleKernel
 
             $files = DB::table('temporary_files')->where('expires_at', '<=', Carbon::now()->add(6, 'hours')->toDateTimeString())
                 ->where('expires_at', '>', Carbon::now()->toDateTimeString())
-                ->where('notified', 0)
                 ->get();
 
             foreach ($files as $booking){
 //                $booking->expert->user->notify(new MeetingReminderNotification());
 //                $booking->customer->user->notify(new MeetingReminderNotification());
-                $booking->notified = 1;
+//                $booking->notified = 1;
                 $booking->save();
             }
         })->hourly();
